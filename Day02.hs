41e7b8b3cc46d3e057f62e465dc4348a5f3bf9b6
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-02 08:44:15 +0100

    Day 02 part two

--- Day02.hs
+++ Day02.hs
@@ -1,6 +1,8 @@
 module Day02 where
 
 import Data.List (sort)
+import Data.Foldable (toList)
+import Data.Sequence (fromList, deleteAt)
 
 safe :: [Int] -> Bool
 safe report = sorted && changing && smallSteps
@@ -9,9 +11,17 @@ safe report = sorted && changing && smallSteps
           changing = all (>0) differences
           smallSteps = all (<= 3) differences
 
+almostSafe :: [Int] -> Bool
+almostSafe = any safe . allDeletions
+    where allDeletions report = map (toList . flip deleteAt (fromList report)) [0..length report]
+
 partOne :: [[Int]] -> Int
 partOne = length . filter safe
+
+partTwo :: [[Int]] -> Int
+partTwo = length . filter almostSafe
 
 main = do
     reports <- map ((map read) . words) . lines <$> readFile "day02.txt"
     print $ partOne reports
+    print $ partTwo reports
