256a5c9a0a94ffa8792063db15fe8e3a2be4abc8
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-08 11:58:53 +0100

    Day 8 part two

--- Day08.hs
+++ Day08.hs
@@ -16,6 +16,18 @@ partOne grid = length $ filter (`M.member` grid) antinodes
           antis ((n1x,n1y),(n2x,n2y)) = [(2*n1x-n2x,2*n1y-n2y),(2*n2x-n1x,2*n2y-n1y)]
           antinodes = nub $ concatMap (concatMap antis . pairs . nodes) frequencies
 
+partTwo :: Grid -> Int
+partTwo grid = length $ filter inLineWithAnyPair $ M.keys grid
+    where frequencies = nub $ M.elems $ M.filter (/= '.') grid
+          nodes f = M.keys $ M.filter (== f) grid
+          pairs n = [(x,y) | (x:ys) <- tails n, y <- ys]
+          allPairs = concatMap (pairs . nodes) frequencies
+          inLineWithAnyPair xy = any (inLine xy) allPairs
+          inLine (x,y) ((n1x,n1y),(n2x,n2y)) = forY == forX && snd forY == 0 && snd forX == 0
+            where forY = (y - n1y) `divMod` (n2y-n1y)
+                  forX = (x - n1x) `divMod` (n2x-n1x)
+
 main = do
     grid <- asMap . lines <$> readFile "day08.txt"
     print $ partOne grid
+    print $ partTwo grid
