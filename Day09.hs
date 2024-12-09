ab5bb1061d24aae0b5ffabfef25a7d03fc004a18
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-09 18:27:30 +0100

    Day 9 part two

--- Day09.hs
+++ Day09.hs
@@ -1,9 +1,10 @@
 module Day09 where
 
 import qualified Data.Sequence as S
+import qualified Data.Map as M
 import Data.Foldable
 import Data.Char (digitToInt)
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isJust)
 import Data.List.HT (takeUntil)
 
 data Block = Free Int | File Int Int deriving (Eq, Show)
@@ -44,13 +45,41 @@ defragStep (firstFree, lastFile, drive, done)
 checksum :: Drive -> Int
 checksum = doCS 0 0 . toList
     where doCS count total [] = total
-          doCS count total (File id len:rest) = doCS (count+len) (total+blockCS) rest
-            where blockCS = sum $ map (id *) $ take len [count..]
+          doCS count total (File fileId len:rest) = doCS (count+len) (total+blockCS) rest
+            where blockCS = sum $ map (fileId *) $ take len [count..]
           doCS count total (Free len:rest) = doCS (count+len) total rest
 
 partOne :: Drive -> Int
 partOne = checksum . defrag
 
+defragTwo :: Drive -> Drive
+defragTwo drive = snd $ foldl defragStepTwo (locs, drive) (reverse fileIds)
+  where locs = M.fromList $ [(id,id*2) | id <- fileIds]
+        fileIds = [0..S.length drive `div` 2]
+        fileLen (Just (File _ len)) = len
+
+defragStepTwo :: (M.Map Int Int, Drive) -> Int -> (M.Map Int Int, Drive)
+defragStepTwo (locs, drive) fileId = if canDefrag then fit else (locs, drive)
+  where canDefrag = isJust firstFreeSpace && fromJust firstFreeSpace < fileLoc
+        firstFreeSpace = S.findIndexL isFreeAndEnough drive
+        isFreeAndEnough (File _ _) = False
+        isFreeAndEnough (Free freeLen) = freeLen >= fileLen
+        Just (Free freeLen) = S.lookup (fromJust firstFreeSpace) drive
+        fileLoc = fromJust $ M.lookup fileId locs
+        (Just (File _ fileLen)) = S.lookup fileLoc drive
+        fit = if freeLen == fileLen then (locs, swapFreeAndFile) else moveFileUp
+        swapFreeAndFile = S.update (fromJust firstFreeSpace) (File fileId fileLen) preSwap
+        preSwap = S.update fileLoc (Free fileLen) drive
+        moveFileUp = (shiftLocs, partiallyFill)
+        shiftLocs = M.map (\i -> if i > fromJust firstFreeSpace then i+1 else i) locs
+        partiallyFill = S.update (fromJust firstFreeSpace) (File fileId fileLen) shortenFree
+        shortenFree = S.insertAt (fromJust firstFreeSpace+1) (Free (freeLen-fileLen)) freeLastFile
+        freeLastFile = S.update fileLoc (Free fileLen) drive
+
+partTwo :: Drive -> Int
+partTwo drive = checksum $ defragTwo drive
+
 main = do
     driveMap <- map digitToInt <$> readFile "day09.txt"
     print $ partOne $ parse driveMap
+    print $ partTwo $ parse driveMap
