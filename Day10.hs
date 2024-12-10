48d957a39fbf89c8e96efbc19119ee88360e63a9
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-10 08:07:34 +0100

    Day 10 part two

--- Day10.hs
+++ Day10.hs
@@ -14,7 +14,7 @@ neighbors :: (Int,Int) -> [(Int,Int)]
 neighbors (x,y) = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 
 trailStep :: Grid -> [(Int,Int)] -> [(Int,Int)]
-trailStep grid = nub . concatMap next
+trailStep grid = concatMap next
     where next (x,y) = filter rises (neighbors (x,y))
                         where rises p = M.findWithDefault '.' p grid == succ (fromJust $ M.lookup (x,y) grid)
 
@@ -22,9 +22,14 @@ trailFrom :: Grid -> (Int, Int) -> [[(Int,Int)]]
 trailFrom grid start = take 10 $ iterate (trailStep grid) [start]
 
 partOne :: Grid -> Int
-partOne grid = sum $ map (length . last . trailFrom grid) starts
+partOne grid = sum $ map (length . nub . last . trailFrom grid) starts
+    where starts = M.keys $ M.filter (=='0') grid
+
+partTwo :: Grid -> Int
+partTwo grid = sum $ map (length . last . trailFrom grid) starts
     where starts = M.keys $ M.filter (=='0') grid
 
 main = do
     grid <- asMap . lines <$> readFile "day10.txt"
     print $ partOne grid
+    print $ partTwo grid
