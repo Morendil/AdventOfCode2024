2b1a8a7e3cfc80109d3fc616ad03600883021e51
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-11 07:50:11 +0100

    Day 11 part two

--- Day11.hs
+++ Day11.hs
@@ -1,8 +1,12 @@
 module Day11 where
 
-step :: [Int] -> [Int]
-step = concatMap stepStone
-    where stepStone 0 = [1]
+import Data.List (nub)
+import qualified Data.Map as M
+
+step :: M.Map Int Int -> M.Map Int Int
+step stones = M.fromListWith (+) $ concatMap stepPair $ M.assocs stones
+    where stepPair (num,total) = map (,total) $ stepStone num
+          stepStone 0 = [1]
           stepStone stone | evenDs = [read (take (ds `div` 2) num), read (drop (ds `div` 2) num)]
             where ds = length num
                   evenDs = even ds
@@ -10,8 +14,15 @@ step = concatMap stepStone
           stepStone stone = [2024*stone]
 
 partOne :: [Int] -> Int
-partOne stones = length $ last $ take 26 $ iterate step stones
+partOne = solve 25
+
+partTwo :: [Int] -> Int
+partTwo = solve 75
+
+solve :: Int -> [Int] -> Int
+solve n = sum . last . take (n+1) . iterate step . M.fromList . map (, 1)
 
 main = do
     stones <- map read . words <$> readFile "day11.txt"
     print $ partOne stones
+    print $ partTwo stones    
