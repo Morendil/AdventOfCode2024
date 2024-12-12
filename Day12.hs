a16b484fbec0cca428a2da8bf3ea9e40d257c3be
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-12 13:44:57 +0100

    Day 12 part two

--- Day12.hs
+++ Day12.hs
@@ -3,7 +3,7 @@ module Day12 where
 import Data.Graph
 import Data.Tree
 import qualified Data.Map as M
-import Data.List (nub)
+import Data.List (nub, tails)
 import Data.Tuple (swap)
 type Pos = (Int, Int)
 type Grid = M.Map Pos Char
@@ -16,6 +16,9 @@ neighbors (x,y) = [(x,y),(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
 
 otherNeighbors :: (Int,Int) -> [(Int,Int)]
 otherNeighbors (x,y) = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+
+offsets :: [(Int,Int)]
+offsets = [(-1,0),(1,0),(0,-1),(0,1)]
 
 findEdges :: Grid -> [Edge]
 findEdges grid = concatMap edgesNs (M.assocs grid)
@@ -31,8 +34,25 @@ perimeter grid ps = length different
           self = M.findWithDefault '.' (head ps) grid
           different = filter (\p -> M.findWithDefault '.' p grid /= self) allNs
 
+sides :: Grid -> [Pos] -> Int
+sides grid ps = length allSides
+    where allSides = map (map(all!!).flatten) $ dff $ buildG (0,length all-1) edgesG
+          edgesG = filter (\(i1,i2) -> sameSide (all !! i1, all !! i2)) $ cross [0..length all-1]
+          sameSide (((px1,py1),o1),((px2,py2),o2)) = o1 == o2 && ((px1==px2 && (abs (py1-py2)<=1)) || (py1==py2 && (abs (px1-px2)<=1)))
+          all = [(p,o) | p@(px,py) <- ps, o@(ox,oy) <- offsets, different (px+ox,py+oy)]
+          cross n = [(x,y) | x <-n, y <- n]
+          self = M.findWithDefault '.' (head ps) grid
+          different n = M.findWithDefault '.' n grid /= self
+
 partOne :: Grid -> Int
 partOne gardens = sum $ zipWith (*) (map length regions) (map (perimeter gardens) regions)
+    where graph = buildG (0, length gardens-1) $ findEdges gardens
+          maxX = maximum $ map snd $ M.keys gardens
+          fromVertex v = swap $ v `divMod` (maxX+1)
+          regions =  map (map fromVertex . flatten) $ dff graph
+
+partTwo :: Grid -> Int
+partTwo gardens = sum $ zipWith (*) (map length regions) (map (sides gardens) regions)
     where graph = buildG (0, length gardens-1) $ findEdges gardens
           maxX = maximum $ map snd $ M.keys gardens
           fromVertex v = swap $ v `divMod` (maxX+1)
@@ -41,3 +61,4 @@ partOne gardens = sum $ zipWith (*) (map length regions) (map (perimeter gardens
 main = do
     gardens <- asMap . lines <$> readFile "day12.txt"
     print $ partOne gardens
+    print $ partTwo gardens
