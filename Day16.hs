c5d83a7d136cbd0dc7edb95f68160270065c29d7
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-16 20:26:41 +0100

    Day 16 part two

--- Day16.hs
+++ Day16.hs
@@ -1,4 +1,4 @@
-module Day16 where
+module Main where
 
 import Algorithm.Search
 
@@ -8,15 +8,14 @@ import Data.Tuple (swap)
 import Data.Maybe (fromJust)
 import Data.List (nub)
 
-import Debug.Trace
-
 type Pos = (Int,Int)
 type Dir = (Int,Int)
 data Move = Step | Turn deriving (Eq, Show, Ord)
 type Step = (Move, Pos, Dir)
 type Grid = M.Map Pos Char
+type CostMap = M.Map Step Int
 
-type Path = (Bool, Int, S.Set Step, Step)
+type Path = (Bool, Int, [Step], Step)
 
 add :: Pos -> Dir -> Pos
 add (x1,y1) (x2,y2) = (x1+x2,y1+y2)
@@ -47,6 +46,34 @@ partOne maze = fst $ fromJust $ dijkstra (nextMoves maze) cost ((==) end.getPos)
     where start = (Step, head $ M.keys $ M.filter ('S' ==) maze, (1,0))
           end = head $ M.keys $ M.filter ('E' ==) maze
 
+advance :: Grid -> Pos -> [Path] -> [Path]
+advance grid goal = concatMap takeSteps
+    where takeSteps done@(True, _, _, _) = [done]
+          takeSteps (False, costSoFar, seen, last) = map takeStep $ nextMoves grid last
+            where takeStep move@(_,pos,_) = (pos==goal, costSoFar + cost last move, move:seen, move)
+
+done :: Path -> Bool
+done (d,_,_,_) = d
+
+allRoutes :: Grid -> [Path]
+allRoutes grid = snd $ solve (M.empty,[(False, 0, [start], start)])
+    where best = partOne grid
+          start = (Step, head $ M.keys $ M.filter ('S' ==) grid, (1,0))
+          end = head $ M.keys $ M.filter ('E' ==) grid
+          solve (seen,paths) = if all done paths then (seen,paths) else iterate (seen,paths)
+          iterate (seen,paths) = let f = filtered seen paths in solve (record seen f,f) -- solve $ traceShow (length f) (record seen f,f)
+          record = foldl (\m (_,c,_,l) -> M.insert l c m)
+          filtered seen paths = filter (\p -> done p || (worth p && not (redundant seen p))) $ advance grid end paths
+          redundant seen (_,c,_,last) = M.findWithDefault (best*2) last seen <= c
+          worth (done,cost,_,_) = cost <= best
+
+getSteps :: Path -> [Step]
+getSteps (_,_,steps,_) = steps
+
+partTwo :: Grid -> Int
+partTwo grid = length $ nub $ map getPos $ concatMap getSteps $ allRoutes grid
+
 main = do
     maze <- M.filter ('.' /=) . asMap . lines <$> readFile "day16.txt"
     print $ partOne maze
+    print $ partTwo maze
