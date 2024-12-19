8543b786c2d0d3e721b4b2aae0a0898b3481134f
Laurent Bossavit <laurent@MacBook-Pro-de-Laurent.local>
2024-12-19 11:16:13 +0100

    Day 19 part two

--- Day19.hs
+++ Day19.hs
@@ -5,7 +5,11 @@ import Data.List (isPrefixOf)
 import Data.Maybe (mapMaybe)
 import Data.Char (isAlpha)
 
+import qualified Data.Map as M
+import Data.List.HT (takeUntil)
+
 type Towels = ([String],[String])
+type Ways = M.Map String Int
 
 parse :: [String] -> Towels
 parse all = (designs $ head all, drop 2 all)
@@ -18,6 +22,20 @@ solve stripes towel = bfs succ (towel ==) ""
 partOne :: Towels -> Int
 partOne (patterns, designs) = length $ mapMaybe (solve patterns) designs
 
+complete :: [String] -> String -> Ways -> Ways
+complete stripes towel ways = M.fromListWith (+) $ alreadyThere ++ concatMap next assocs
+    where next (prefix, count) = map (,count) $ filter (`isPrefixOf` towel) $ map (prefix ++) stripes
+          alreadyThere = filter (\(prefix, count) -> prefix == towel) assocs
+          assocs = M.assocs ways
+
+howMany :: [String] -> String -> Int
+howMany stripes towel = sum $ M.elems $ last $ takeUntil (\ways -> null ways || M.keys ways == [towel]) $ steps
+    where steps = iterate (complete stripes towel) $ M.fromList [("",1)]
+
+partTwo :: Towels -> Int
+partTwo (patterns, designs) = sum $ map (howMany patterns) designs
+
 main = do
     towels <- parse . lines <$> readFile "day19.txt"
     print $ partOne towels
+    print $ partTwo towels
